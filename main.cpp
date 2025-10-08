441de619bd66ccdc6461fa46b8a03734d898483c
SirChili <thomastony4628@gmail.com>
2025-10-08 01:56:45 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,92 @@
 #include <iostream>
 #include <iomanip>
+#include <conio.h>
+#include <ctime>
+#include <Windows.h> //Sleep function
 
 using namespace std;
 
 const char dark = 0xB2;
+int position = 15;
 
+//Map Creation
 void map()
 {
-
-	cout << "\n";
-
-	for (int i = 0; i < 30; i++)
-	{
-		cout << " " << setw(5);
-		cout << dark << setw(40);
-		cout << dark << "\n";
+	for (int i = 0; i < 30; i++) {
+		cout << dark << setw(30) << dark << "\n";
 	}
+}
 
+void enemies() {
+	//This is randomized but plays as the player, change it to play as enemies instead
+	while (true) {
+
+		char key = _getch();
+
+		if (key == 'q') {
+			break;
+		}
+
+		srand(time(0));
+		int random = rand() % 30;
+		Sleep(700);
+		system("cls");
+		map();
+
+		cout << setw(random) << "#";
+
+	}
 	
+}
+
+//Players Movement
+void plr() {
+
+	cout << setw(position) << "#";
+	
+	while(true){
+		
+		char key = _getch();
+		
+		if (key == 'a')
+		{ 
+			if (position == 1) {
+				system("cls");
+				cout << "Out Of Bounds...\n\nGame Over\n";
+				break;
+			}
+
+			system("cls");
+			map();
+			position--;
+		
+			cout << setw(position) << "#";
+		}
+		if (key == 'd')
+		{ 
+			if (position == 31) {
+				system("cls");
+				cout << "Out Of Bounds...\n\nGame Over\n";
+				break;
+
+			}
+			system("cls");
+			map();
+			position++; 
+
+			cout << setw(position) << "#";
+		}
+		if (key == 'q'){break;}
+			
+		
+	}
 
 }
 
-
+//Calling the functions
 int main() {
 
 	map();
+	plr();
 }
+
